00b4ff640f62389ffc650443c901b9d15532e777
sososimsim <phewphewsimsim@naver.com>
2017-08-16 18:21:05 +0900

    더미 마크업 추가하기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main id="tt-wrapper">
+        <section id="today-info">
+          <h2 className="today-date">오늘은<br/><b>8월 7일</b><br/><b>월요일</b>입니다</h2>
+        </section>
+        <section id="add-todays-todo">
+          <input placeholder="오늘의 할 일을 추가해주세요" type="text"/>
+          <button type="button" name="add-todo-button">+</button>
+        </section>
+        <section id="todays-todo">
+          <h4>#Todays_Todo</h4>
+          <ul>
+            <li className="todays-todo completed">
+              <input name="todays-todo" type="checkbox" className="toggle"/>
+              <label htmlFor="todays-todo">스토어팜 이슈 확인하기</label>
+              <button type="button" className="delete">X</button>
+            </li>
+            <li className="todays-todo">
+              <input name="todays-todo" type="checkbox" className="toggle"/>
+              <label htmlFor="todays-todo">점심 회식</label>
+              <button type="button" className="delete">X</button>
+            </li>
+            <li className="todays-todo">
+              <input name="todays-todo" type="checkbox" className="toggle"/>
+              <label htmlFor="todays-todo">리액트 스터디 과제하기</label>
+              <button type="button" className="delete">X</button>
+            </li>
+          </ul>
+        </section>
+        <section id="tried-todo">
+          <h4>#Tried_Todo</h4>
+          <ul>
+            <li className="tried-todo">
+              <input name="tried-todo" type="checkbox" className="toggle"/>
+              <label htmlFor="tried-todo">테이블 야자에 꽂을 무언가 찾기</label>
+              <span className="tried-date">17.08.08</span>
+            </li>
+            <li className="tried-todo">
+              <input name="tried-todo" type="checkbox" className="toggle"/>
+              <label htmlFor="tried-todo">TT 마크업 짜기</label>
+              <span className="tried-date">17.08.09</span>
+            </li>
+          </ul>
+        </section>
+      </main>
     );
   }
 }

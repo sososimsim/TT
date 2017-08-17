4e74768beb4bcc6f2398a35409ac8d12262d4964
sososimsim <phewphewsimsim@naver.com>
2017-08-18 01:13:19 +0900

    [03] TodayInfo가 제대로 오늘 날짜 정보를 표시하도록 만듭니다

--- src/component/TodayInfo.js
+++ src/component/TodayInfo.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import '../styles/TodayInfo.css';
 
+const DAY = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"];
+
 class TodayInfo extends Component {
+
+  _getMonthString(){
+    return (new Date().getMonth() + 1) + "월";
+  }
+
+  _getDateString(){
+    return new Date().getDate() + "일";
+  }
+
+  _getDayString(){
+    const now = new Date();
+    const dayIndex = now.getDay() - 1;
+    return DAY[dayIndex];
+  }
+
   render(){
     return(
       <section id="today-info">
-        <h2 className="today-date">오늘은<br/><b>8월 7일</b><br/><b>월요일</b>입니다</h2>
+        <h2 className="today-date">오늘은<br/>
+        <b>{this._getMonthString()} {this._getDateString()}</b><br/>
+        <b>{this._getDayString()}</b>입니다
+        </h2>
       </section>
     );
   }

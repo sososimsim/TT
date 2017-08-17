f6743c39590dc5166fd0e3e4cca77c84fd6d5e23
shinyoung-park <park.shinyoung@navercorp.com>
2017-08-17 19:19:43 +0900

    [02] completed 여부에 따라 completed 클래스를 추가할 때, 추가하지 않아야하는 경우에 false로 추가되는 것을 공백으로 수정합니다

--- src/component/TodaysTodo.js
+++ src/component/TodaysTodo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class TodaysTodo extends Component {
   render(){
     return(
-      <li className={"todays-todo " + (this.props.todo.completed && "completed")}>
+      <li className={"todays-todo" + (this.props.todo.completed ? " completed" : "")}>
         <input name="todays-todo" type="checkbox" className="toggle"/>
         <label htmlFor="todays-todo">{this.props.todo.content}</label>
         <button type="button" className="delete">X</button>

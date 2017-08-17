29896f98cc089d7ac989ec227420044a55370b49
shinyoung-park <park.shinyoung@navercorp.com>
2017-08-17 18:46:36 +0900

    [02] today, tried todo 구분하는 필터를 추가합니다

--- src/App.js
+++ src/App.js
@@ -5,14 +5,98 @@ import TodoRegister from './component/TodoRegister';
 import TodaysTodos from './component/TodaysTodos';
 import TriedTodos from './component/TriedTodos';
 
+const mockData = {
+  todos: [
+   // 오늘이 8월 10일(Aug 10 2017)인 상태,
+   { // #Todays_todo 완료된 todo
+     completed: true,
+     registeredDate: new Date(),
+     completedDate: new Date(),
+     content: "리액트 스터디 과제하기"
+   },
+   { // #Todays_todo 아직 완료하지 않은 todo
+     completed: false,
+     registeredDate: new Date(),
+     completedDate: null,
+     content: "책상 정리하기"
+   },
+   { // 화면에 보이지 않는, 과거에 완료한 todo
+     completed: true,
+     registeredDate: new Date("8/10/2017"),
+     completedDate: new Date("8/10/2017"),
+     content: "책 반납하기"
+   },
+   { // #Tried_todo에 노출되는, 오늘보다 이전에 등록되었는데(registeredDate이 오늘 이전) 아직 완료하지 않은 todo
+     completed: false,
+     registeredDate: new Date("8/10/2017"),
+     completedDate: null,
+     content: "사진 출력하기"
+   }
+ ]
+};
+
 class App extends Component {
+  constructor(props){
+    super();
+    this.todoFilter = this.todoFilter.bind(this);
+  }
+
+  todoFilter(filter){
+    const todos = mockData;
+    switch (filter) {
+      case "today":
+        //registeredDate가 오늘인 친구들
+        return this._getTodaysTodos();
+      case "tried":
+        //registeredDate가 오늘 이전인데, completed가 false인 친구들
+        return this._getTriedTodos();
+      default:
+        return todos;
+    }
+  }
+
+  _getTodaysTodos(){
+    const todos = mockData.todos;
+    return todos.filter(this._isToday);
+  }
+
+  _getTriedTodos(){
+    const todos = mockData.todos;
+    const pastTodos = todos.filter(function(todo){
+      if (this._isToday(todo)) {
+        return false;
+      } else {
+        return true;
+      }
+    }.bind(this));
+    return pastTodos.filter(this._isNotCompleted);
+  }
+
+  _isNotCompleted(todo){
+    return todo.completed === false;
+  }
+
+  _isToday(todo){
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+
+    const registeredDate = todo.registeredDate;
+    const registeredYear = registeredDate.getFullYear();
+    const registeredMonth = registeredDate.getMonth() + 1;
+    const registeredDay = registeredDate.getDate();
+
+    return (year === registeredYear) && (month === registeredMonth) && (day === registeredDay);
+  }
+
   render() {
     return (
       <main id="tt-wrapper">
         <TodayInfo/>
         <TodoRegister/>
-        <TodaysTodos/>
-        <TriedTodos/>
+        <TodaysTodos todaysTodos={this.todoFilter("today")}/>
+        <TriedTodos triedTodos={this.todoFilter("tried")}/>
       </main>
     );
   }

754ae67991f75446e29e07452a941e5ce4b32a45
sososimsim <phewphewsimsim@naver.com>
2017-08-21 21:59:15 +0900

    [04] TodoRegister에서 엔터키로 입력한 할일을 today's todo에 추가할 수 있도록 합니다

--- src/component/TodoRegister.js
+++ src/component/TodoRegister.js
@@ -25,10 +25,23 @@ class TodoRegister extends Component {
     this.props.onRegister(newTodoData);
   }
 
+  onEnter = (e) => {
+    if (e.which === 13){
+      let newTodoData = {
+        completed: false,
+        registeredDate: new Date(),
+        completedDate: null,
+        content: this.state.newTodo
+      };
+      this.props.onRegister(newTodoData);
+    }
+  }
+
   render(){
     return (
       <section id="add-todays-todo">
-        <input placeholder="오늘의 할 일을 추가해주세요" type="text" value={this.state.newTodo} onChange={this.onChange}/>
+        <input placeholder="오늘의 할 일을 추가해주세요" type="text" value={this.state.newTodo}
+          onChange={this.onChange} onKeyUp={this.onEnter}/>
         <button type="button" name="add-todo-button" onClick={this.onClickButton}>+</button>
       </section>
     );

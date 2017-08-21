4632871aafeae8d67e918a318d7374e30b44a9b3
sososimsim <phewphewsimsim@naver.com>
2017-08-21 22:29:56 +0900

    [05] Today's todo에서 완료 상태를 체크할 수 있도록 합니다

--- src/component/TodaysTodo.js
+++ src/component/TodaysTodo.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class TodaysTodo extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      completed: props.todo.completed
+    }
+  }
+
+  toggleCompleteState = () => {
+    this.setState({
+      completed: !this.state.completed
+    });
+  }
+
   render(){
     return(
-      <li className={"todays-todo" + (this.props.todo.completed ? " completed" : "")}>
-        <input name="todays-todo" type="checkbox" className="toggle"/>
+      <li className={"todays-todo" + (this.state.completed ? " completed" : "")}>
+        <input name="todays-todo" type="checkbox" className="toggle" onClick={this.toggleCompleteState}/>
         <label htmlFor="todays-todo">{this.props.todo.content}</label>
         <button type="button" className="delete">X</button>
       </li>

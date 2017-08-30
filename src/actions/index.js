b9a69e9ed116c82fc0991151502986b088a17278
sososimsim <phewphewsimsim@naver.com>
2017-08-31 01:39:21 +0900

    [07] 오타 수정

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 //actions 디렉토리만 불러왔을 때에 바로 이 ActionCreator가 로드되도록 하기 위해 파일 이름을 index.js로 짓습니다.
 
 //미리 정의해두었던 action들을 불러옵니다.
-import * as actions from './ActionTypes';
+import * as actions from './actionTypes';
 
-//적절한 액션을 돌려주는 액션생성자ActionCreator들을 정의하고, export합니다. 
+//적절한 액션을 돌려주는 액션생성자ActionCreator들을 정의하고, export합니다.
 export function addTodaysTodo(todo){
   return {
     type: actions.ADD_TODAYS_TODO,
@@ -18,8 +18,10 @@ export function toggleTodaysTodo(todo){
       todo: todo
     }
   } else {
+    return {
       type: actions.COMPLETE_TODAYS_TODO,
       todo: todo
+    }
   }
 }
 
@@ -37,7 +39,9 @@ export function toggleTriedTodo(todo){
       todo: todo
     }
   } else {
+    return {
       type: actions.RESET_TO_NOT_COMPLETED_TRIED_TODO,
       todo: todo
+    }
   }
 }

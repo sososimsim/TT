57a09116094b353119f03f192a6e144ea765ba6f
sososimsim <phewphewsimsim@naver.com>
2017-08-31 00:59:48 +0900

    [-] defaultTodos 데이터 추가

--- src/reducers/todaysTodo.js
+++ src/reducers/todaysTodo.js
@@ -2,8 +2,34 @@
 import * as actions from '../actions/ActionTypes';
 
 const defaultTodos = {
-  todaysTodos: [],
-  triedTodos: []
+  todaysTodos: [
+    { // #Todays_todo 완료된 todo
+      completed: true,
+      registeredDate: new Date(),
+      completedDate: new Date(),
+      content: "리액트 스터디 과제하기"
+    },
+    { // #Todays_todo 아직 완료하지 않은 todo
+      completed: false,
+      registeredDate: new Date(),
+      completedDate: null,
+      content: "책상 정리하기"
+    }
+  ],
+  triedTodos: [
+    { // 화면에 보이지 않는, 과거에 완료한 todo
+      completed: true,
+      registeredDate: new Date("8/10/2017"),
+      completedDate: new Date("8/10/2017"),
+      content: "책 반납하기"
+    },
+    { // #Tried_todo에 노출되는, 오늘보다 이전에 등록되었는데(registeredDate이 오늘 이전) 아직 완료하지 않은 todo
+      completed: false,
+      registeredDate: new Date("8/10/2017"),
+      completedDate: null,
+      content: "사진 출력하기"
+    }
+  ]
 };
 
 export default todaysTodo(todos = defaultTodos, action) {

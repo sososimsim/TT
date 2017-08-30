8e871e4f257f97520f64b54b1862db0234a20f95
sososimsim <phewphewsimsim@naver.com>
2017-08-31 02:47:13 +0900

    [07] 동일한 방식으로 TriedTodos의 props를 redux와 연결합니다.

--- src/App.js
+++ src/App.js
@@ -5,75 +5,11 @@ import TodoRegister from './component/TodoRegister';
 import TodaysTodos from './component/TodaysTodos';
 import TriedTodos from './component/TriedTodos';
 
-const defaultTodos = [
-  // 오늘이 8월 10일(Aug 10 2017)인 상태,
-  { // #Todays_todo 완료된 todo
-    completed: true,
-    registeredDate: new Date(),
-    completedDate: new Date(),
-    content: "리액트 스터디 과제하기"
-  },
-  { // #Todays_todo 아직 완료하지 않은 todo
-    completed: false,
-    registeredDate: new Date(),
-    completedDate: null,
-    content: "책상 정리하기"
-  },
-  { // 화면에 보이지 않는, 과거에 완료한 todo
-    completed: true,
-    registeredDate: new Date("8/10/2017"),
-    completedDate: new Date("8/10/2017"),
-    content: "책 반납하기"
-  },
-  { // #Tried_todo에 노출되는, 오늘보다 이전에 등록되었는데(registeredDate이 오늘 이전) 아직 완료하지 않은 todo
-    completed: false,
-    registeredDate: new Date("8/10/2017"),
-    completedDate: null,
-    content: "사진 출력하기"
-  }
-];
-
 class App extends Component {
   constructor(props){
-    super();
+    super(props);
     this.onRegisterTodo = this.onRegisterTodo.bind(this);
-    this.state = {
-      todaysTodos: this._getTodaysTodos(defaultTodos),
-      triedTodos: this._getTriedTodos(defaultTodos)
-    };
-  }
-
-  _getTodaysTodos(todos){
-    return todos.filter(this._isToday);
-  }
-
-  _getTriedTodos(todos){
-    const pastTodos = todos.filter(function(todo){
-      if (this._isToday(todo)) {
-        return false;
-      } else {
-        return true;
-      }
-    }.bind(this));
-    return pastTodos.filter(this._isNotCompleted);
-  }
-
-  _isNotCompleted(todo){
-    return todo.completed === false;
-  }
-
-  _isToday(todo){
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
-
-    const registeredDate = todo.registeredDate;
-    const registeredYear = registeredDate.getFullYear();
-    const registeredMonth = registeredDate.getMonth() + 1;
-    const registeredDay = registeredDate.getDate();
-
-    return (year === registeredYear) && (month === registeredMonth) && (day === registeredDay);
+    // this.state = this.props.store.getState();
   }
 
   onRegisterTodo(newTodo){
@@ -116,10 +52,9 @@ class App extends Component {
         <TodayInfo/>
         <TodoRegister onRegister={this.onRegisterTodo}/>
         <TodaysTodos updateTodaysTodo={this.updateTodaysTodo} deleteTodo={this.deleteTodaysTodo}/>
-        <TriedTodos triedTodos={this.state.triedTodos} updateTriedTodo={this.updateTriedTodo}/>
+        <TriedTodos updateTriedTodo={this.updateTriedTodo}/>
       </main>
     );
   }
 }
-
 export default App;
--- src/component/TriedTodos.js
+++ src/component/TriedTodos.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import '../styles/TriedTodos.css';
 import TriedTodo from './TriedTodo';
+import { connect } from 'react-redux';
 
 class TriedTodos extends Component {
   constructor(props){
     super();
   }
 
+  _getTriedTodos(todos){
+    const pastTodos = todos.filter(function(todo){
+      if (this._isToday(todo)) {
+        return false;
+      } else {
+        return true;
+      }
+    }.bind(this));
+    return pastTodos.filter(this._isNotCompleted);
+  }
+
+  _isNotCompleted(todo){
+    return todo.completed === false;
+  }
+
+  _isToday(todo){
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+
+    const registeredDate = todo.registeredDate;
+    const registeredYear = registeredDate.getFullYear();
+    const registeredMonth = registeredDate.getMonth() + 1;
+    const registeredDay = registeredDate.getDate();
+
+    return (year === registeredYear) && (month === registeredMonth) && (day === registeredDay);
+  }
+
   render(){
+    let triedTodos = this._getTriedTodos(this.props.triedTodos);
     return (
       <section id="tried-todo">
         <h4>#Tried_Todo</h4>
         <ul>
-          {this.props.triedTodos.map((todo, id) => {
+          {triedTodos.map((todo, id) => {
             return <TriedTodo todo={todo} key={id} id={id} updateTriedTodo={this.props.updateTriedTodo}/>
           })}
         </ul>
@@ -21,4 +52,10 @@ class TriedTodos extends Component {
   }
 }
 
-export default TriedTodos;
+const mapStateToProps = (state) => {
+  return {
+    triedTodos: state.triedTodos
+  }
+}
+
+export default connect(mapStateToProps)(TriedTodos);
